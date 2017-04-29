cf9bf283308d22b10ae53c333e31edcc5c131de9
Itai Edri <itaied246@gmail.com>
2017-04-29 15:58:38 +0300

    Add Maybe usage example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-const user :user = {
+import * as _ from "ramda";
+import { User } from "./user/user";
+import { Maybe } from "ramda-fantasy";
+
+const itai :User = {
     id: 1,
-    name: "Itai Edri"
+    name: "Itai Edri",
+    address: {
+        street: {
+            name: "Rotchild",
+            number: 100
+        }
+    }
 }
 
-function main() {
-    console.log(user);
+const lidor :User = {
+    id: 2,
+    name: "Lidor Cohen"
 }
+
+const main = () => {
+    console.log(Maybe.maybe("Address is empty", 
+                            _.identity, 
+                            getStreetNumber(itai)));
+    // 100                            
+
+    console.log(Maybe.maybe("Address is empty", 
+                            _.identity, 
+                            getStreetNumber(lidor)));
+    // Address is empty                             
+}
+
+// getStreetNumber :: User => Maybe number
+const getStreetNumber = _.compose(_.map(_.prop("number")), 
+                                  _.map(_.prop("street")), 
+                                  Maybe.toMaybe, 
+                                  _.prop("address"));
+    
 
 main();
--- src/user/user.ts
+++ src/user/user.ts
@@ -1,4 +1,14 @@
-interface user {
+export interface User {
     id: number;
+    name: string;
+    address?: address;
+}
+
+interface address {
+    street: street;
+}
+
+interface street {
+    number: number;
     name: string;
 }
